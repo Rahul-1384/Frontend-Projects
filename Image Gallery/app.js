c78718f235517e0177fd9d28f205cee3b0397107
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-12-28 11:44:30 +0530

    animation effect

---
+++ Image Gallery/app.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }else{
+            entry.target.classList.remove("show");
+        }
+    })
+})
+
+const card = document.querySelectorAll(".card");
+card.forEach((el) => {
+    observer.observe(el)
+});
