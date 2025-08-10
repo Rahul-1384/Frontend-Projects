08b02723286654caf37e5c17ae64b3be09cac414
Rahul Rajput <156775643+rahul1384r@users.noreply.github.com>
2025-08-10 15:21:27 +0530

    feat: Slider practice

--- Practice HTML&CSS/script.js
+++ Practice HTML&CSS/script.js
@@ -25,3 +25,36 @@ const images = new IntersectionObserver((entries) => {
 document.querySelectorAll(".right-hero").forEach((el) => {
     images.observe(el)
 })
+
+
+// Slider
+const img = document.querySelector(".img-slide");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let imagesArray = ['photo1.jpg', 'photo2.jpg'];
+let currentIdx = 0;
+
+function nextSlide(){
+    currentIdx++;
+    if(currentIdx >= imagesArray.length){
+        currentIdx = 0;
+    }
+    showImage(currentIdx);
+}
+function prevSlide(){
+    currentIdx--;
+    if(currentIdx < 0){
+        currentIdx = imagesArray.length - 1;
+    }
+    showImage(currentIdx);
+}
+function showImage(idx){
+    img.style.opacity = 0;
+    setTimeout(() => {
+        img.src = imagesArray[idx];
+        img.style.opacity = 1;
+    }, 300)
+}
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
